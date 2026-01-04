3f2f0a118e9be22bc1896d1dbb6c4425ab419ea0
Abhinav Shrivastav <your_email@example.com>
2026-01-04 12:22:57 +0530

    add: budget.py

---
+++ budget.py
@@ -0,0 +1,14 @@
+budget = 5000.0
+students = 20
+
+# 1. Create a variable called 'cost_per_student' 
+# (Divide budget by students)
+cost_per_student = budget/students
+
+# 2. Create a boolean called 'is_affordable' 
+# (Check if cost_per_student is less than 300)
+is_affordable =cost_per_student<300
+
+# 3. Print both results
+print("Cost per student is:", cost_per_student)
+print("Is it affordable?", is_affordable)
