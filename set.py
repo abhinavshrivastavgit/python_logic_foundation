34e7bb926ed20097906793f26d5eccb470c8de85
Abhinav Shrivastav <your_email@example.com>
2026-01-05 00:31:41 +0530

    add: set.py

---
+++ set.py
@@ -0,0 +1,7 @@
+workshop_1 = {"Sophia", "Vaishnavi", "Arjun"}
+workshop_2 = {"Vaishnavi", "aryan"}
+
+both = workshop_1.intersection(workshop_2)
+unions = workshop_1.union(workshop_2)
+print(both)
+print(unions)
