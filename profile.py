0b755080560e7c2584cc5507e3e56853d13ba11f
Abhinav Shrivastav <your_email@example.com>
2026-01-04 12:33:12 +0530

    add: profile.py

---
+++ profile.py
@@ -0,0 +1,13 @@
+# 1. Create your profile list
+profile = ["IEEE Leader",25,"Python"]
+
+
+# 2. ADD your city to the list (The Append Move)
+profile.append("Gurugram")
+
+# 3. UPDATE your age (The Index Move)
+# Since you are 25, let's pretend it's your birthday and make it 26
+profile[1] = 26
+
+# 4. PRINT the final profile LIST
+print(f'My Updated profile: {profile}')
