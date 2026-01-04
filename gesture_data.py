124ea8389a273b5d6efdffb4741463abcbcb2b07
Abhinav Shrivastav <your_email@example.com>
2026-01-05 00:17:02 +0530

    Add: gesture_data.py

---
+++ gesture_data.py
@@ -0,0 +1,11 @@
+#key : Values
+gesture_data = {
+    "label" : "Peace sign" , 
+    "id" : 5,
+    "coordinates" : (10.5, 20.9) #created a tuple 
+}
+
+detected_signs = {"Peace Sign" , "Thumbs Up", "Peace Sign"}
+
+print(gesture_data)
+print(detected_signs)
