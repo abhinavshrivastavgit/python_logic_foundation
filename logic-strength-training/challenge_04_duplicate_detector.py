a73735a15c87db9fd46f82df98392a7d2fe7b248
Abhinav Shrivastav <your_email@example.com>
2026-01-08 17:57:58 +0530

    feat: add challenge_04_duplicate_detector.py

--- logic-strength-training/challenge_04_duplicate_detector.py
+++ logic-strength-training/challenge_04_duplicate_detector.py
@@ -1,5 +1,6 @@
+# challenge_04_duplicate_detector.py
 list_A = ["Alice","Bob","Charlie"]
-list_B = ["Bob", "David", "Alice"]
+list_B = ["Bob", "David", "Alice "]
 
 for name in list_A:
     if name in list_B:
