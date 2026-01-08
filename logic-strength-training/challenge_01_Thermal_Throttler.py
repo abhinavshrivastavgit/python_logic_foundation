494f9a99f3c052540e4bcaf96fef51e0692da033
Abhinav Shrivastav <your_email@example.com>
2026-01-08 15:16:15 +0530

    feat: add challenge_01_Thermal_throttler.py

---
+++ logic-strength-training/challenge_01_Thermal_Throttler.py
@@ -0,0 +1,17 @@
+#challenge_01_Thermal_throttler.py
+
+import random
+
+gpu_temps = []
+
+temp_count = int(input('Enter the number of interval you want result for: '))
+
+for i in range(0, temp_count):
+    temp_value = random.randint(50,95)
+    gpu_temps.append(temp_value)
+
+for temp in gpu_temps:
+    if temp >80:
+        print(f'Slow Down your CPU temp is: {temp}')
+    else:
+     print(f'Keep running your CPU temp is:{temp}')
